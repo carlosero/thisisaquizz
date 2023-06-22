297f0e0b9818fb20c5b4d147537182ed94ecd4cd
Carlos Rodriguez <ecarlos.rodriguez@gmail.com>
2023-06-22 15:34:34 -0500

    Change api url

--- src/services/quiz-api.js
+++ src/services/quiz-api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = 'https://thisisaquizz-dev-qn4tol5xia-ue.a.run.app/'
-const API_URL = 'http://localhost:3000'
+const API_URL = 'https://thisisaquizz-dev-qn4tol5xia-ue.a.run.app/'
+// const API_URL = 'http://localhost:3000'
 
 const QuizApi = {
 	createQuiz: async () => {
